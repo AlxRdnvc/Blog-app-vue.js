95f261a2afa4fc69912e151510722197a6c19ff9
Aleksandar_Radanovic <a.radanovic1984@gmail.com>
2018-08-16 20:03:33 +0200

    zadatak 2

--- src/pages/AppPosts.vue
+++ src/pages/AppPosts.vue
@@ -1,9 +1,31 @@
 <template>
-    <h1>POSTOVI</h1>
+    <div>
+        <h1 style="text-align: center;">Posts</h1><br><br>
+        <div v-for="(post, index) in posts" :key="index"> 
+            <h2 style="padding-left: 5%">{{ post.title }}</h2><br>
+            <p style="padding-left: 5%">{{ post.text }}</p><hr>
+        </div>
+    </div>
 </template>
 <script>
+
+import {posts} from '../services/posts.js'
+
 export default {
-    name: 'posts'
+    name: 'posts',
+    data() {
+        return {
+            posts: []
+        }
+    },
+    beforeRouteEnter (to, from, next) {
+    posts.getAll()
+      .then((response) => {
+          next((vm) => {
+            vm.posts = response.data
+          })
+    })
+  }
 }
 </script>
 
